5fd54743830de33d69e8a13c24a9faa3d8ecce36
Kuro Arigami <kuro@localhost.localdomain>
2019-04-28 14:15:16 -0400

    call on function by id

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var triangleType =function (){
+    var Length = parseFloat(document.getElementById('Length').value);
+    var Height = parseFloat(document.getElementById('Height').value);
+    var Base = parseFloat(document.getElementById('Base').value);
+    
+}

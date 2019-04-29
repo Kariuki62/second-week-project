510f5d043aa72fcae21e22864359afe30259a4a6
Kuro Arigami <kuro@localhost.localdomain>
2019-04-29 01:14:14 -0400

    Add uf statement equilateral

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-var triangleType =function (){
+var button= document.getElementById('Type');
+
+var triangleType =function home(){
     var Length = parseFloat(document.getElementById('Length').value);
     var Height = parseFloat(document.getElementById('Height').value);
     var Base = parseFloat(document.getElementById('Base').value);
+
+    console.log(Length);
+    console.log(Height);
+    console.log(Base);
+
+    if( Length === Height && Height=== Base && Base=== Length){
+        alert("Triangle is equilateral");
+}
+else if(Length === Height && Height!== Base)
     
+}
+if(button){
+    button.addEventListener('click',triangleType);
 }

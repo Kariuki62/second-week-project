62880f323c6fbf2a25e763e8ad8405524dc569bc
Kuro Arigami <kuro@localhost.localdomain>
2019-04-29 01:20:51 -0400

    Add else if statement isosceles

--- js/script.js
+++ js/script.js
@@ -12,8 +12,12 @@ var triangleType =function home(){
     if( Length === Height && Height=== Base && Base=== Length){
         alert("Triangle is equilateral");
 }
-else if(Length === Height && Height!== Base)
-    
+else if(Length === Height && Height !== Base){
+    alert("Triangle is Isosceles");
+}
+else if(Height === Length && Length !== Base){
+    alert("Triangle is Isosceles");
+}
 }
 if(button){
     button.addEventListener('click',triangleType);
